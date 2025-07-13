5e95aaa7def910356babe7b225a2dd9c8c93f377
Богдан Кузьменко <bogdan.kuzmenko@lred.ru>
2025-07-14 09:54:54 +1000

    test

--- cookies.ts
+++ cookies.ts
@@ -1,6 +1,6 @@
 import css from "./cookies.css?inline"
 
-(function (): null | undefined {
+((): null | undefined => {
     function setCookie(name: string, value: string, days: number): void {
         const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
