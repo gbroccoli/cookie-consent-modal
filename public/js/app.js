5348560b8f7a20af469d923bbc385d5e10d423c3
Богдан Кузьменко <bogdan.kuzmenko@lred.ru>
2025-07-14 17:25:13 +1000

    edit

--- public/js/app.js
+++ public/js/app.js
@@ -31,3 +31,58 @@ document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
         }
     });
 });
+
+/**
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days
+ * @return {void}
+ */
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value}; path=/; expires=${date.toUTCString()}`;
+}
+
+/**
+ * @param {string} name
+ * @return {null|string}
+ */
+function getCookie(name) {
+
+    /**
+     * @type {string[]}
+     */
+    const cookies = document.cookie.split('; ');
+    for (const cookie of cookies) {
+        const [key, val] = cookie.split('=');
+        if (key === name) return val;
+    }
+    return null;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cookies = document.querySelector('#cookies');
+
+    if (cookies) {
+        if (getCookie("cookieAccepted") === 'true') {
+            cookies.remove()
+        } else {
+            cookies.classList.remove('noshow')
+            const btnSuccess = cookies.querySelector('.cookies-btn-success')
+            const btnClose = cookies.querySelector('.cookies-btn-close')
+
+            if (btnClose && btnSuccess) {
+
+                btnClose.addEventListener('click', (e) => {
+                    window.close()
+                })
+
+                btnSuccess.addEventListener('click', (e) => {
+                    setCookie('cookieAccepted', 'true', 365)
+                    cookies.remove()
+                })
+            }
+        }
+    }
+})
